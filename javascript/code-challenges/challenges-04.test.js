90abf83289e63f13739f80e555d09258a277574a
Junyoung Son <junyoungson808@gmail.com>
2022-08-19 20:22:35 -0700

    lab4

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -40,6 +40,9 @@ For example: 'Cat' would come before 'apple'
 
 const sortNames = (arr) => {
   // Solution code here...
+  return arr.sort((v,i) => {
+    if( )
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
