79a0141f4d98d4deb70f7b5d999c2c407d5f9309
Junyoung Son <junyoungson808@gmail.com>
2022-09-30 23:16:35 +0000

    update

--- javascript/stack-and-queue/stack.js
+++ javascript/stack-and-queue/stack.js
@@ -1,5 +1,12 @@
 'use strict';
 
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Stack {
   constructor(){
     this.top = null;
@@ -7,16 +14,21 @@ class Stack {
 
   push(value){
     let newNode = new Node;
-    newNode = ;
-
+    newNode.next = this.top;
+    this.top = newNode;
   }
 
   pop(){
-
+    // Arguments: none
+    // Returns: the value from node from the top of the stack
+    // Removes the node from the top of the stack
+    // Should raise exception when called on empty stack
   }
 
   peek(){
-    //solve this-----
+    // Arguments: none
+    // Returns: Value of the node located at the top of the stack
+    // Should raise exception when called on empty stack
   }
 
   isEmpty(){
@@ -47,18 +59,18 @@ class Queue{
   // Returns: the value from node from the front of the queue
   // Removes the node from the front of the queue
   // Should raise exception when called on empty queue
+
   peek(){
-    
+
   }
   // Arguments: none
   // Returns: Value of the node located at the front of the queue
   // Should raise exception when called on empty stack
+
   isEmpty(){
-    
+
   }
   // Arguments: none
   // Returns: Boolean indicating whether or not the queue is empty
-
-
 }
 
