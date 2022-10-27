11320e60ce4bb78036ba95529ff3326ade97fb55
Junyoung Son <junyoungson808@gmail.com>
2022-10-27 02:21:54 +0000

    code ch 29

--- javascript/code-challenges/401-code-challenges/sorting/sort-filter-movies/sort.js
+++ javascript/code-challenges/401-code-challenges/sorting/sort-filter-movies/sort.js
@@ -2,66 +2,43 @@
 
 const movies = require('./movies');
 
-// write your own comparator function for each function and export THOSE separately for testing as well. You will also use comparators in the three pre-written functions bellow 
-
-// examples for demo
-const books = ['A Tale of Two Cities', 'Mary Poppins', 'Lord of the Rings'];
-
-// sample:
-let sampleComparator = (a, b) => {
-  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-  if (nameA < nameB) {
+let sortYearComparator = (a, b) => {
+  if (a.year < b.year) {
     return -1;
   }
-  if (nameA > nameB) {
+  if (a.year > b.year) {
     return 1;
   }
-
-  // names must be equal
   return 0;
-}
+};
 
-// comparator for books:
-function bookComparator(a, b){
-  const bookA = a.replace('A ', '');
-  const bookB = b.replace('A ', '');
+console.log(movies);
 
-  if(bookA < bookB) return -1;
-  if(bookA > bookB) return 1;
+let sortTitleComparator = (a, b) => {
+  const titleA = a.title.replace('The ', '');
+  const titleB = b.title.replace('The ', '');
+
+  if (titleA < titleB) {
+    return -1;
+  }
+  if (titleA > titleB) {
+    return 1;
+  }
   return 0;
-
-}
-
-console.log(books);
-books.sort(bookComparator);
-console.log(books);
-
-const standings = [
-  {team: 'Lions', score: 4},
-  {team: 'Tigers', score: 1},
-  {team: 'Bears', score: 5},
-  {team: 'OhMy...', score: 3},
-];
-
-//Demo:  stay tuned...
-
-// what is a comparator?
-// function that determines sort definitions
-
+};
 
 
 // Complete for Code Challenge 28
 function sortYear(movies) {
-  return [];
+  return movies.sort(sortYearComparator);
 }
 
 function sortTitle(movies) {
-  return [];
+  return movies.sort(sortTitleComparator);
 }
 
 function inGenre(movies, genre) {
-  return [];
+  return movies.filter(movie => movie.genres.includes(genre));
 }
 
-module.exports = { sortYear, sortTitle, inGenre }
+module.exports = { sortYear, sortTitle, inGenre };
