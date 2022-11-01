821ba3f623d4b923cf859a89f5b34556f3928cbf
Junyoung Son <junyoungson808@gmail.com>
2022-11-01 05:11:27 +0000

    codech31

---
+++ javascript/code-challenges/401-code-challenges/repeated-word/README.md
@@ -0,0 +1,21 @@
+# Repeated word
+<!-- Short summary or background information -->
+RepeatedWord Function will take a string as a input, and look for the first word to be repeated. Once it has found the word it will return the word.
+
+## Challenge
+<!-- Description of the challenge -->
+Create a function called repeated word that finds the first word to occur more than once in a string.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available in each of your hashtable -->
+Repeated Words:
+Write a function called repeated word that finds the first word to occur more than once in a string
+Arguments: string
+Return: string
+
+## Links
+[Folkstalk.com](https://www.folkstalk.com/tech/how-to-find-repeated-characters-in-a-string-in-javascript-with-code-examples/)
+[]()
---
+++ javascript/code-challenges/401-code-challenges/repeated-word/repeated-word.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let str = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+
+const repeatedWords = (str) => {
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return str[i];
+      }
+    }
+  }
+  return -1;
+};
+
+console.log(repeatedWords(str));
